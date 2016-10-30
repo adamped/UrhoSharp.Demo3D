f13084c2c07cb8cf153447f2d2786620d3a3143a
Adam Pedley <adam.pedley@gmail.com>
2016-10-30 18:33:24 +1100

    Working code

--- UrhoSharp.Demo/UrhoSharp.Demo/HelloWorld.cs
+++ UrhoSharp.Demo/UrhoSharp.Demo/HelloWorld.cs
@@ -7,27 +7,45 @@ namespace UrhoSharp.Demo
 {
     public class HelloWorld : Application
     {
-        protected override void Start()
+        protected override async void Start()
         {
             base.Start();
 
-            CreateScene();
+            await Create3DObject();
         }
 
-        private void CreateScene()
+        private async Task Create3DObject()
         {
-            // Create Text Element
-            var text = new Text()
-            {
-                Value = "Hello World!",
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center
-            };
+            // Scene
+            var scene = new Scene();
+            scene.CreateComponent<Octree>();
 
-            text.SetColor(Color.Cyan);
-            text.SetFont(font: ResourceCache.GetFont("Fonts/Anonymous Pro.ttf"), size: 30);
-            // Add to UI Root
-            UI.Root.AddChild(text);
+            // Node (Rotation and Position)
+            Node node = scene.CreateChild();
+            node.Position = new Vector3(0, 0, 5);
+            node.Rotation = new Quaternion(10, 60, 10);
+            node.SetScale(1f);
+
+            // Pyramid Model
+            StaticModel modelObject = node.CreateComponent<StaticModel>();
+            modelObject.Model = ResourceCache.GetModel("Models/Pyramid.mdl");
+            
+            // Light
+            Node light = scene.CreateChild(name: "light");
+            light.SetDirection(new Vector3(0.4f, -0.5f, 0.3f));
+            light.CreateComponent<Light>();
+            
+            // Camera
+            Node cameraNode = scene.CreateChild(name: "camera");
+            Camera camera = cameraNode.CreateComponent<Camera>();
+
+            // Viewport
+            Renderer.SetViewport(0, new Viewport(scene, camera, null));
+
+            // Action
+            await node.RunActionsAsync(
+                new RepeatForever(new RotateBy(duration: 1,
+                    deltaAngleX: 0, deltaAngleY: 90, deltaAngleZ: 0)));
 
         }
 
