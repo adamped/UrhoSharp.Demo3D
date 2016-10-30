02072699b0669813238a27d1fd8ac94310b45368
Adam Pedley <adam.pedley@gmail.com>
2016-10-30 18:32:57 +1100

    Rotating Pyramid

---
+++ UrhoSharp.Demo/UrhoSharp.Demo/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Urho;
+using Urho.Actions;
+using Urho.Gui;
+
+namespace UrhoSharp.Demo
+{
+    public class HelloWorld : Application
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            CreateScene();
+        }
+
+        private void CreateScene()
+        {
+            // Create Text Element
+            var text = new Text()
+            {
+                Value = "Hello World!",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            text.SetColor(Color.Cyan);
+            text.SetFont(font: ResourceCache.GetFont("Fonts/Anonymous Pro.ttf"), size: 30);
+            // Add to UI Root
+            UI.Root.AddChild(text);
+
+        }
+
+    }
+}
---
+++ UrhoSharp.Demo/UrhoSharp.Demo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Urho;
+using Xamarin.Forms;
+
+namespace UrhoSharp.Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await HelloWorldUrhoSurface.Show<HelloWorld>(new Urho.ApplicationOptions(assetsFolder: null) { Orientation = ApplicationOptions.OrientationType.LandscapeAndPortrait });
+
+
+        }
+    }
+}
